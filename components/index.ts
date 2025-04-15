60e8007d0ae55dbfbc692e317762bcbdf4130278
Vipul10708 <vipulreddy10@gmail.com>
2025-04-14 20:53:16 -0500

    Footer ,Navbar and mapping from constants in footer comp

--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,10 @@
 import Hero from "./hero";
 import CustomButton from "./custombutton";
+import Footer from "./footer";
+import Navbar from "./navbar";
 export {
     Hero,
     CustomButton,
+    Footer,
+    Navbar,
 }
